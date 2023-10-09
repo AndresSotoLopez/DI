f7b0f41bc29684691b5e9732f62849831ecf1c3f
AndresSotoLopez <asotol22@fpcoruna.afundacion.org>
2023-10-09 17:43:51 +0200

    Sprint1 Tarea #6, cambio de nombres en carpeta unedited y añadido codigo para reescale de imagenes

--- sprint1Tkinter/catalogo/window.py
+++ sprint1Tkinter/catalogo/window.py
@@ -1,6 +1,7 @@
 from tkinter import ttk, messagebox
 import tkinter as tk
 from cell import Cell
+from PIL import Image, ImageTk
 
 class MainWindow():
 
@@ -14,16 +15,20 @@ class MainWindow():
 
         self.cells = [
 
-            Cell("COD 4", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/edited/cod4.png"),
-            Cell("COD MW2", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/edited/codmw2.png"),
-            Cell("COD MW3", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/edited/codmw3.png"),
-            Cell("COD BO2", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/edited/codbo2.png"),
-            Cell("COD BO3", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/edited/codbo3.png"),
+            Cell("COD 4", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/unedited/cod4.png"),
+            Cell("COD MW2", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/unedited/codmw2.png"),
+            Cell("COD MW3", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/unedited/codmw3.png"),
+            Cell("COD BO2", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/unedited/codbo2.png"),
+            Cell("COD BO3", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/unedited/codbo3.png"),
 
 
         ]
 
         for i, cell in enumerate(self.cells):
-            label = ttk.Label(root, image= cell.imageTk, text=cell.title, compound=tk.BOTTOM)
-            label.grid(row=0, column=1+i)
-            label.bind("<Button-1>", lambda event, celda = cell: self.on_button_clicked(celda))
+           
+         
+            img = (Image.open(cell.path)).resize((100, 100), Image.Resampling.LANCZOS)
+            cell.imagetk = ImageTk.PhotoImage(img)
+            label = ttk.Label(root, image = cell.imagetk, text = cell.title, compound = tk.BOTTOM)
+            label.grid(row = 0,column = i)
+            label.bind("<Button-1>",lambda event, cell = cell: self.onButtonClicked(cell))
