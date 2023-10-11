5e33d849326ea9c71d01db6546e2617e581ef829
AndresSotoLopez <asotol22@fpcoruna.afundacion.org>
2023-10-11 09:17:54 +0200

    Sprint1 #Tarea7, nueva ventana con evento clickable

--- sprint1Tkinter/catalogo/cell.py
+++ sprint1Tkinter/catalogo/cell.py
@@ -3,9 +3,10 @@ from PIL import Image, ImageTk
 
 class Cell:
 
-    def __init__(self, title, path):
+    def __init__(self, title, path, description):
 
         self.title = title
         self.path = path
+        self.description = description
         img = (Image.open(self.path)).resize((100, 100), Image.Resampling.LANCZOS)
         self.imageTk = ImageTk.PhotoImage(img)
--- sprint1Tkinter/catalogo/window.py
+++ sprint1Tkinter/catalogo/window.py
@@ -1,25 +1,25 @@
 from tkinter import ttk, messagebox
 import tkinter as tk
 from cell import Cell
-from PIL import Image, ImageTk
+from detail_window import DetailWindow
 
 class MainWindow():
 
     def on_button_clicked(self, cell):
-        message = "Has hecho click en la celda " + cell.title
-        messagebox.showinfo("Información: ", message)
+        detail_window = DetailWindow(self.root, cell.title, cell.imageTk, cell.description)
 
     def __init__(self, root):
         
+        self.root = root
         root.title("MainWindow")
 
         self.cells = [
 
-            Cell("COD 4", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/unedited/cod4.png"),
-            Cell("COD MW2", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/unedited/codmw2.png"),
-            Cell("COD MW3", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/unedited/codmw3.png"),
-            Cell("COD BO2", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/unedited/codbo2.png"),
-            Cell("COD BO3", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/unedited/codbo3.png"),
+            Cell("COD 4", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/unedited/cod4.png", "COD4"),
+            Cell("COD MW2", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/unedited/codmw2.png", "CODMW2"),
+            Cell("COD MW3", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/unedited/codmw3.png", "CODMW3"),
+            Cell("COD BO2", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/unedited/codbo2.png", "CODBO2"),
+            Cell("COD BO3", "/home/drew/DAM/Desarrollo de interfaces/DI/sprint1Tkinter/catalogo/data/unedited/codbo3.png", "CODBO3"),
 
 
         ]
@@ -28,4 +28,4 @@ class MainWindow():
           
             label = ttk.Label(root, image = cell.imageTk, text = cell.title, compound = tk.BOTTOM)
             label.grid(row = 0,column = i)
-            label.bind("<Button-1>",lambda event, cell = cell: self.onButtonClicked(cell))
+            label.bind("<Button-1>",lambda event, cell = cell: self.on_button_clicked(cell))
