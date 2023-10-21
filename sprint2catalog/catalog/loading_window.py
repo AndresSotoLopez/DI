4f5fcb72cf7220822d9e420f86581006f4bc7cc4
AndresSotoLopez <asotol22@fpcoruna.afundacion.org>
2023-10-21 13:18:50 +0200

    Añadidas funciones a loading_window

--- sprint2catalog/catalog/loading_window.py
+++ sprint2catalog/catalog/loading_window.py
@@ -1,15 +1,25 @@
 import threading
+import requests
 from tkinter import Tk
 import tkinter as tk
+from window import MainWindow
 
 class loading_window:
     
+    
+    
     def __init__(self, root):
+        
+        #Declaracion de variables/
         self.root=root
         self.root.title("[!] Cargando....")
         self.root.geometry("170x120")
         self.root.resizable(False, False)
+        self.finished = False
+        self.json_data = []
+        self.progress=0
         
+        #Nuevo label a la hora de descargar los datos de GitHub
         self.label = tk.Label(self.root, text="[!] Cargando datos....", font=("Arial", 12))
         self.label.pack()
         
@@ -18,14 +28,10 @@ class loading_window:
         self.canvas = tk.Canvas(self.root, width=60, height=60, bg=label_bg_color)
         self.canvas.pack()
         
-        self.progress=0
-        
+        #Creacion/actualizacion del circulo de carga
         self.draw_progress_circle(self.progress)
         
         self.update_progress_circle()
-        
-        #self.thread = threading.Thread(target=self.fetch_json_data)
-        #self.thread.start()
         
     def draw_progress_circle(self, progress):
         
@@ -43,4 +49,22 @@ class loading_window:
         self.draw_progress_circle(self.progress)
         self.root.after(100, self.update_progress_circle)
         
+    def launch_main_window(json_data):
+        root = Tk()
+        app = MainWindow(root, json_data)
+        root.mainloop()    
         
+    def feth_json_data(self):
+        response = requests.get("https://raw.githubusercontent.com/AndresSotoLopez/DI/main/recursos/catalog.json")
+        if response.status_code == 200:
+            self.json_data = response.json()
+            self.finished=True
+                      
+    def check_thread(self):
+        if(self.finished):
+            self.root.destroy()
+            launch_main_window(self.json_data)
+        else:
+            self.root.adter(100, self.check_thread)
+            
+    
--- sprint2catalog/catalog/main.py
+++ sprint2catalog/catalog/main.py
@@ -1,11 +1,8 @@
 from tkinter import Tk
-from window import MainWindow
 from loading_window import loading_window
 
 if __name__ == "__main__":
-
-    root = Tk()
-    app = loading_window(root)
-    #app = MainWindow(root)
-    root.mainloop()
   
+  root = Tk()
+  app = loading_window(root)
+  root.mainloop()
