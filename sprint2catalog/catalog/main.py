56ec265d52d1456b889da67c3fa7c2b856096954
AndresSotoLopez <asotol22@fpcoruna.afundacion.org>
2023-10-21 12:40:44 +0200

    Tarea #2 sprint 3, añadida ventana de carga

--- sprint2catalog/catalog/main.py
+++ sprint2catalog/catalog/main.py
@@ -1,8 +1,11 @@
 from tkinter import Tk
 from window import MainWindow
+from loading_window import loading_window
 
 if __name__ == "__main__":
 
     root = Tk()
-    app = MainWindow(root)
+    app = loading_window(root)
+    #app = MainWindow(root)
     root.mainloop()
+  
